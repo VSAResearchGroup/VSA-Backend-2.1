2a8f9a3e8e6c5a107fd870c9f0aa427031faa25f
AndrueCashman <mastasphinx@hotmail.com>
2018-12-08 21:58:49 -0800

    Fixed a bug where MachineNodes were being duplicated
    reverted some other changes that are different than dev branch

---
+++ Algorithm-2.0-master/Algorithm-2.0-master/DBConnection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.IO;
+
+
+namespace Scheduler
+{
+    class DBConnection
+    {
+        SqlConnection myConnection;      //Declare the SQL connection to Database
+
+        #region SQL Stuff
+        //------------------------------------------------------------------------------
+        // concise code to execute queries
+        // 
+        // 
+        //------------------------------------------------------------------------------
+        
+        public DataTable ExecuteToDT(string query)
+        {
+            OpenSQLConnection();
+            SqlCommand cmd = new SqlCommand(query, myConnection);
+            DataTable dt = new DataTable();
+            using (var con = myConnection)
+            {
+                using (var command = new SqlCommand(query))
+                {
+                    myConnection.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dt.Load(dr);
+                    }
+                }
+            }
+            myConnection.Close();
+            return dt;
+        }
+
+        public string ExecuteToString(string query)
+        {
+            
+            OpenSQLConnection();
+            SqlCommand cmd = new SqlCommand(query, myConnection);
+            myConnection.Open();
+            var result = new StringBuilder();
+            var reader = cmd.ExecuteReader();
+            if (!reader.HasRows)
+            {
+                result.Append("[]");
+            }
+            else
+            {
+                while (reader.Read())
+                {
+                    result.Append(reader.GetValue(0).ToString());
+                }
+            }
+            myConnection.Close();
+            return result.ToString();  
+        }
+        //------------------------------------------------------------------------------
+        // sql connection
+        // 
+        // 
+        //------------------------------------------------------------------------------
+        private void OpenSQLConnection()
+        {
+
+            myConnection = new SqlConnection("Data Source=65.175.68.34;Initial Catalog=vsaDev;Persist Security Info=True;User ID=sa;Password=H4ZXZy-vRZwL#9A");
+        }
+        #endregion
+
+    }
+}
---
+++ Algorithm-2.0-master/Algorithm-2.0-master/Deserializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler {
+    class Deserializer {
+        private String json;
+
+        public Deserializer(String str) {
+            json = str;
+        }
+
+        public Object getString() {
+            return null;
+        }
+    }
+}
