2f7f661571ac3ca349652b38c1955b882571c15c
Rigdha Acharya <riach@microsoft.com>
2019-02-19 20:44:45 -0800

    wip

--- VaaApi/Controllers/PreferencesController.cs
+++ VaaApi/Controllers/PreferencesController.cs
@@ -21,7 +21,29 @@ namespace VaaApi.Controllers
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         public string Post(CourseObject content)
         {
-            return "done";
+            try
+            {
+                var DBPlugin = new DBConnection();
+                var majorId = Convert.ToInt32(content.major);
+                var schoolId = Convert.ToInt32(content.school);
+                var job = Convert.ToInt32(content.job);
+                var enrollment = Convert.ToInt32(content.enrollment);
+                var coreCourses = Convert.ToInt32(content.courses);
+                var quarters = Convert.ToInt32(content.quarters);
+                var creditPerQuarter = Convert.ToInt32(content.credits);
+                var summer = content.summer;
+                DBPlugin.ExecuteToString($"insert into ParameterSet (MajorId, SchoolId, JobTypeID, TimePreferenceId, QuarterPreferenceId, DateAdded, NumberCoreCoursesPerQuarter, " +
+                                         $"MaxNumberOfQuarters, CreditsPerQuarter, SummerPreference, EnrollmentTypeId, Status, LastDateModified) values ({majorId}, {schoolId}, {job}, {1}, {1}, '{DateTime.UtcNow}', {coreCourses}," +
+                                         $"{quarters}, {creditPerQuarter}, '{summer}', {enrollment}, {1}, '{DateTime.UtcNow}')");
+                var insertedId = DBPlugin.ExecuteToString("SELECT IDENT_CURRENT('ParameterSet')");
+                return insertedId.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+           
         }
 
 
---
+++ VaaApi/Controllers/RatingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using Scheduler;
+
+namespace VaaApi.Controllers
+{
+    using System.Web.Http.Cors;
+    using System.Web.Http.Results;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class RatingController : ApiController
+    {
+        // GET api/values
+        [SwaggerOperation("Post")]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public string Post(JObject content)
+        {
+            var id = (string) content["id"];
+            var rating = (JObject) content["rating"];
+            var advisorRating = (string)rating["schedule-rating"];
+            var advisorInt = Convert.ToInt32(advisorRating);
+            var dbhelper = new DBConnection();
+            dbhelper.ExecuteToString($"Update GeneratedPlan Set AdvisorScore={advisorInt} where GeneratedPlanID={id}");
+            return "done";
+        }
+
+
+
+    }
+
+    
+}
